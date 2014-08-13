8dcc2179675677e2d227b0cc6d883f36791b54b0
stef-ang <anggara.stefanus@gmail.com>
2014-08-13 17:18:11 +0700

    12/08/2014-17:17
    Check pertama

--- DCupNote/DCupNote/Program.cs
+++ DCupNote/DCupNote/Program.cs
@@ -16,7 +16,7 @@ namespace DCupNote
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new MainForm());
         }
     }
 }
